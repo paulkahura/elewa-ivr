b81ad3d0a60a97b3aea0bf179e9f4cda8e6036d3
k0imburi <pablokahura@gmail.com>
2024-10-25 17:33:54 +0300

    Update twilio-handle-incoming-calls.handler.ts

--- libs/functions/bot-engine/interactive-voice-response/src/lib/twilio-handle-incoming-calls.handler.ts
+++ libs/functions/bot-engine/interactive-voice-response/src/lib/twilio-handle-incoming-calls.handler.ts
@@ -3,14 +3,26 @@ import { Twilio, validateRequest } from "twilio/lib";
 
 import { HandlerTools } from '@iote/cqrs';
 import { FunctionContext, FunctionHandler, HttpsContext, RestResult } from '@ngfi/functions';
+import { ActiveChannel, BlockDataService, ChannelDataService, ConnectionsDataService, CursorDataService, EngineBotManager, generateEndUserId, MessagesDataService, StoriesDataService } from '@app/functions/bot-engine';
+
 
 import { TwilioIVRRequest } from './models/twilio-ivr-request.interface';
+import { __ConvertIvrApiPayload } from "./utils/convert-ivr-payload.util";
+import { IVRActiveChannel } from "./models/ivr-active-channel.class";
+import { EndUser } from "@app/model/convs-mgr/conversations/chats";
+import { Cursor, PlatformType } from "@app/model/convs-mgr/conversations/admin/system";
+import { Message, MessageDirection } from "@app/model/convs-mgr/conversations/messages";
+import { MessageTypes } from "@app/model/convs-mgr/functions";
+import { PlayService } from "./models/ivr-play.service";
+import { IVRStoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+import { BotEnginePlay } from "libs/functions/bot-engine/main/src/lib/services/bot-engine-play.service";
+import { ProcessMessageService } from "libs/functions/bot-engine/main/src/lib/services/process-message/process-message.service";
+import { BotMediaProcessService } from "libs/functions/bot-engine/main/src/lib/services/media/process-media-service";
 
 
 export class TwilioIncomingCallHandler extends FunctionHandler<any, any> {
-  private twilioClient: Twilio;
 
-  public async execute(data: TwilioIVRRequest, context: HttpsContext, tools: HandlerTools): Promise<RestResult> {
+  public async execute(data: TwilioIVRRequest | null , context: HttpsContext, tools: HandlerTools): Promise<any> {
     // Validate the incoming request is from Twilio
     tools.Logger.debug(() => `Twilio handler hit with dat ${JSON.stringify(context.eventContext.request.query)}`);
     // if (!this.validateRequest(data, context)) {
@@ -19,54 +31,88 @@ export class TwilioIncomingCallHandler extends FunctionHandler<any, any> {
     //     message: 'Invalid Twilio signature'
     //   };
     // }
-    return this.handleCall(data, tools, context);
+    const payload = context.eventContext.request.query;
+    return await this.handleCall(data, payload, tools, context);
   }
 
   private validateRequest(data: any, context: any): boolean {
-    const twilioSignature = context.headers['x-twilio-signature'];
+    const twilioSignature = context.eventContext.request.headers['x-twilio-signature'];
     const url = context.url;
 
     return validateRequest(
-      process.env['TWILIO_AUTH_TOKEN']!,
+      '1ed46f8ab05875419fe28ac3b09cbfa1',
       twilioSignature,
       url,
       data
     );
   }
 
-  async handleCall(req: any, tools: HandlerTools, context: any): Promise<any> {
-    const twimlResponse = new twiml.VoiceResponse();
-    tools.Logger.debug(() => `Twilio handler hit with dat ${JSON.stringify(req.body)}`);
-    // Check if there's user input
-    // const digits = req.body.Digits;
-    const digits = context.eventContext.request.query.digits;
+  async handleCall(req: any, payload: any, tools: HandlerTools, context: any): Promise<any> {
+    const sanitizedResponse = __ConvertIvrApiPayload(payload);
+    const dtmfDigits = context.eventContext.request.query.digits;
 
-    if (!digits) {
-      // Initial greeting and options
-      twimlResponse.say('Welcome to FarmBetter IVR! Please select your preferred voice option to continue:');
-      twimlResponse.gather({
-        numDigits: 1,
-        action: '/voice-selection',
-        method: 'POST'
-      }).say('Press 1 for Male or Press 2 for Female.');
-    }
-    else {
-      // Handle user input
-      switch (digits) {
-        case '1':
-          console.log('we are 1');
-          twimlResponse.say({ voice: 'man' }, 'You have selected the male voice. How can I assist you today?');
-          break;
-        case '2':
-          twimlResponse.say({ voice: 'woman' }, 'You have selected the female voice. How can I assist you today?');
-          break;
-        default:
-          console.log('we are default');
-          twimlResponse.say('Invalid selection. Please try again.');
-          twimlResponse.redirect('/voice');
-      }
+    if (!sanitizedResponse || !sanitizedResponse.type) return { status: 500, message: `Unexpected Payload :: ${JSON.stringify(payload)}` } as RestResult;
+    const _channelService$ = new ChannelDataService(tools, sanitizedResponse);
+
+    const communicationChannel = await _channelService$.getChannelInfo(sanitizedResponse.platformId);
+    if (!communicationChannel) {
+      tools.Logger.error(() => `[ChannelInfo].getChannelInfo - This phone number has not been registered to a channel :: ${sanitizedResponse.platformId}`);
+
+      return { status: 500 } as RestResult;
     }
 
-    return twimlResponse.toString();
+    tools.Logger.log(() => `[ChannelInfo].getChannelInfo - Channel Information acquired successfully: ${JSON.stringify(communicationChannel)}`);
+
+    const ivrActiveChannel = new IVRActiveChannel(tools, communicationChannel);
+
+    // STEP 5: Create the bot engine and process the message.
+    //        Since we receive different types of messages e.g. text message, location,
+    const engine = new EngineBotManager(tools, tools.Logger, ivrActiveChannel);
+    const storyService = new StoriesDataService(tools, communicationChannel.orgId);
+    const connectionDataService = new ConnectionsDataService(communicationChannel, tools);
+    const blockService = new BlockDataService(communicationChannel, connectionDataService, tools);
+    const cursorService = new CursorDataService(tools);
+    const processMediaService = new BotMediaProcessService(tools);
+    const messageService = new MessagesDataService(tools);
+    const processMessageService = new ProcessMessageService(cursorService, connectionDataService, blockService, tools, ivrActiveChannel, processMediaService);
+
+    const bot = new BotEnginePlay(processMessageService, cursorService, messageService,processMediaService, ivrActiveChannel, tools);
+    
+    const story = await storyService.getStory(communicationChannel.defaultStory);
+
+    const ivrEndUser: EndUser = {
+      id: generateEndUserId(sanitizedResponse.endUserNumber, PlatformType.IVR, communicationChannel.n),
+      phoneNumber: sanitizedResponse.endUserNumber,
+    }
+    let block: IVRStoryBlock;
+
+    if(!dtmfDigits)
+    {
+      block = await blockService.getFirstBlock(communicationChannel.orgId, story.id) as IVRStoryBlock;
+  
+      sanitizedResponse.message.text = block.message;
+      sanitizedResponse.payload = block;
+  
+    }
+    else 
+    {
+      const cursor = await cursorService.getLatestCursor(ivrEndUser.id, communicationChannel.orgId) as Cursor;
+      // const blockMessage = await bot.__getNextBlock(ivrEndUser, cursor);
+      block = await blockService.getBlockById(cursor.position.blockId, communicationChannel.orgId, cursor.position.storyId);
+
+      sanitizedResponse.message.text = block.message;
+      sanitizedResponse.payload = block;
+
+    }
+    const result = await engine.run(sanitizedResponse.message, ivrEndUser);
+    // const result = await engine.run({"type": sanitizedResponse.type}, ivrEndUser);
+  
+    console.log("$$$$$$$$$$$$$$$$$$$$$", result);
+
+    return result.data && result.data?.success ?  result.data.toString() : this.systemErrorTwiml();
+  }
+  private systemErrorTwiml()
+  {
+    return new twiml.VoiceResponse().say("Leaving Goomza, 420 Error");
   }
 }
